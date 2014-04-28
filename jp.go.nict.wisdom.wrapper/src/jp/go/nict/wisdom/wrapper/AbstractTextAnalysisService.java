59c8aa238e7734182aaf1e0e87e48b880bb01acb
Masahiro Tanaka <mtnk@nict.go.jp>
2014-04-29 00:06:46 +0900

    Improved efficiecy of parallel execution

--- jp.go.nict.wisdom.wrapper/src/jp/go/nict/wisdom/wrapper/AbstractTextAnalysisService.java
+++ jp.go.nict.wisdom.wrapper/src/jp/go/nict/wisdom/wrapper/AbstractTextAnalysisService.java
@@ -1,19 +1,19 @@
 /*
-* Copyright (C) 2014 Information Analysis Laboratory, NICT
-*
-* RaSC is free software: you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 2.1 of the License, or (at
-* your option) any later version.
-*
-* RaSC is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
-* General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2014 Information Analysis Laboratory, NICT
+ *
+ * RaSC is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * RaSC is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package jp.go.nict.wisdom.wrapper;
 
@@ -35,313 +35,313 @@ import jp.go.nict.wisdom.wrapper.status.ServiceInitialize;
 import jp.go.nict.wisdom.wrapper.status.ServiceStatus;
 
 public abstract class AbstractTextAnalysisService implements TextAnalysisService, ServiceStatus, ServiceInitialize {
-	private static Logger logger = Logger.getLogger(AbstractTextAnalysisService.class.getName());
+    private static Logger logger = Logger.getLogger(AbstractTextAnalysisService.class.getName());
 
-	protected static final AtomicInteger numCmd = new AtomicInteger();
+    protected static final AtomicInteger numCmd = new AtomicInteger();
 
-	protected static BlockingQueue<StringIOCommand> cmdPool = new LinkedBlockingQueue<>();
-	private String directory = null;
-	private String cmdLine;
-	private List<String> cmdArray;
-	private String delimiterIn;
-	private String delimiterOut;
-	private boolean delLastNewline = false;
-	private boolean includeDelim = false;
-	private int pollTimeOut = 3000;
-	private int poolSize = 10;
-	private int initPoolSize = 1;
-	private int timeOut = 60000;
-	private int startWait = 1000;
-	private int restartWait = 1000;
-	private int bufSize = 2000000;
+    protected static BlockingQueue<StringIOCommand> cmdPool = new LinkedBlockingQueue<>();
+    private String directory = null;
+    private String cmdLine;
+    private List<String> cmdArray;
+    private String delimiterIn;
+    private String delimiterOut;
+    private boolean delLastNewline = false;
+    private boolean includeDelim = false;
+    protected int pollTimeOut = 3000;
+    protected int poolSize = 10;
+    protected int initPoolSize = 1;
+    protected int timeOut = 60000;
+    protected int startWait = 1000;
+    protected int restartWait = 1000;
+    protected int bufSize = 2000000;
 
-	public AbstractTextAnalysisService() {
+    public AbstractTextAnalysisService() {
+    }
+
+    protected abstract StringIOCommand getInstance() throws IOException, InterruptedException;
+
+    protected StringIOCommand getInstance(Class<? extends StringIOCommand> cmdClass) throws IOException, InterruptedException {
+	logger.info("getInstance(...)");
+	if(cmdLine != null && cmdArray != null){
+	    logger.severe("Both cmdLine and CmdArray is set. Set either one.");
+	    throw new IOException("Both cmdLine and CmdArray is set. Set either one.");
 	}
+	String[] exeCmd;
+	if(cmdLine != null)
+	    exeCmd = cmdLine.split(" ");
+	else
+	    exeCmd = cmdArray.toArray(new String[0]);
 
-	protected abstract StringIOCommand getInstance() throws IOException, InterruptedException;
+	StringIOCommand cmd = null;
+	if (cmdPool.isEmpty() && numCmd.getAndIncrement() < poolSize) {
+	    int num = numCmd.get();
+	    // Create new process
+	    try {
+		Class<?>[] argType = {String[].class, String.class, String.class, String.class, boolean.class, boolean.class, int.class, int.class, int.class, int.class};
+		Constructor<? extends StringIOCommand> constructor;
+		constructor = cmdClass.getConstructor(argType);
+		cmd = constructor.newInstance(exeCmd, directory, delimiterIn, delimiterOut, delLastNewline, includeDelim, timeOut, startWait, restartWait, bufSize);
+	    } catch (InstantiationException | IllegalAccessException
+		     | IllegalArgumentException | InvocationTargetException
+		     | NoSuchMethodException | SecurityException e) {
+		logger.severe(e.getMessage());
+		throw new RuntimeException(e);
+	    }
+	    cmd.start();
 
-	protected StringIOCommand getInstance(Class<? extends StringIOCommand> cmdClass) throws IOException, InterruptedException {
-		logger.info("getInstance(...)");
-		if(cmdLine != null && cmdArray != null){
-			logger.severe("Both cmdLine and CmdArray is set. Set either one.");
-			throw new IOException("Both cmdLine and CmdArray is set. Set either one.");
+	    logger.info("Created a new process. Pooled processes : " + num);
+	} else {
+	    int num = numCmd.get();
+	    if(num >= Integer.MAX_VALUE - poolSize){
+		numCmd.set(poolSize);
+		logger.config("numCmd reset");
+	    }
+	    // Reuse running process
+	    try {
+		cmd = cmdPool.poll(pollTimeOut, TimeUnit.MILLISECONDS);
+		if(cmd == null){
+		    String msg = "All running processes are occupied. Timeout occured.";
+		    logger.info(msg);
+		    throw new IOException(msg);
 		}
-		String[] exeCmd;
-		if(cmdLine != null)
-			exeCmd = cmdLine.split(" ");
-		else
-			exeCmd = cmdArray.toArray(new String[0]);
-
-		StringIOCommand cmd = null;
-		if (cmdPool.isEmpty() && numCmd.getAndIncrement() < poolSize) {
-			int num = numCmd.get();
-			// Create new process
-			try {
-				Class<?>[] argType = {String[].class, String.class, String.class, String.class, boolean.class, boolean.class, int.class, int.class, int.class, int.class};
-				Constructor<? extends StringIOCommand> constructor;
-				constructor = cmdClass.getConstructor(argType);
-				cmd = constructor.newInstance(exeCmd, directory, delimiterIn, delimiterOut, delLastNewline, includeDelim, timeOut, startWait, restartWait, bufSize);
-			} catch (InstantiationException | IllegalAccessException
-					| IllegalArgumentException | InvocationTargetException
-					| NoSuchMethodException | SecurityException e) {
-				logger.severe(e.getMessage());
-				throw new RuntimeException(e);
-			}
-			cmd.start();
-
-			logger.info("Created a new process. Pooled processes : " + num);
-		} else {
-			int num = numCmd.get();
-			if(num >= Integer.MAX_VALUE - poolSize){
-				numCmd.set(poolSize);
-				logger.config("numCmd reset");
-			}
-			// Reuse running process
-			try {
-				cmd = cmdPool.poll(pollTimeOut, TimeUnit.MILLISECONDS);
-				if(cmd == null){
-					String msg = "All running processes are occupied. Timeout occured.";
-					logger.info(msg);
-					throw new IOException(msg);
-				}
-				cmd.clear();
-				logger.finest("Reused a process");
-			} catch (InterruptedException e) {
-				logger.info("Polling of pooled process was interrupted.");
-				throw e;
-			}
-		}
-		return cmd;
+		cmd.clear();
+		logger.finest("Reused a process");
+	    } catch (InterruptedException e) {
+		logger.info("Polling of pooled process was interrupted.");
+		throw e;
+	    }
 	}
+	return cmd;
+    }
 
-	protected void returnInstance(StringIOCommand cmd) {
-		cmdPool.add(cmd);
-		logger.finest("Retuned a process to pool.");
-	}
+    protected void returnInstance(StringIOCommand cmd) {
+	cmdPool.add(cmd);
+	logger.finest("Retuned a process to pool.");
+    }
 
-	@Override
+    @Override
 	public String analyze(String text) throws Exception {
-		long startMillis = System.currentTimeMillis();
-		StringIOCommand cmd = getInstance();
-		String result = null;
-		try {
-			cmd.put(text);
-			result = cmd.getNextResult();
-		} catch (InterruptedException | IOException e) {
-			logger.severe(e.getMessage());
-			throw e;
-		} finally {
-			returnInstance(cmd);
-			logger.config("Time : [" + (System.currentTimeMillis() - startMillis) + "ms]");
-		}
-		return result;
+	long startMillis = System.currentTimeMillis();
+	StringIOCommand cmd = getInstance();
+	String result = null;
+	try {
+	    cmd.put(text);
+	    result = cmd.getNextResult();
+	} catch (InterruptedException | IOException e) {
+	    logger.severe(e.getMessage());
+	    throw e;
+	} finally {
+	    returnInstance(cmd);
+	    logger.config("Time : [" + (System.currentTimeMillis() - startMillis) + "ms]");
 	}
+	return result;
+    }
 
-	@Override
+    @Override
 	public String[] analyzeArray(String[] text) throws Exception {
-		long startMillis = System.currentTimeMillis();
-		StringIOCommand cmd = getInstance();
-		CatchException ce = new CatchException();
+	long startMillis = System.currentTimeMillis();
+	StringIOCommand cmd = getInstance();
+	CatchException ce = new CatchException();
 
-		Thread inputWorker = new Thread(new InputWorker(
-				cmd, text));
-		inputWorker.setUncaughtExceptionHandler(ce);
-		inputWorker.start();
+	Thread inputWorker = new Thread(new InputWorker(
+							cmd, text));
+	inputWorker.setUncaughtExceptionHandler(ce);
+	inputWorker.start();
 
-		String[] result = new String[text.length];
-		int count = 0;
-		try {
-			while (count < text.length) {
-				result[count++] = cmd.getNextResult();
-			}
-		} catch (InterruptedException | IOException e) {
-			logger.severe(e.getMessage());
-			throw e;
-		} finally {
-			returnInstance(cmd);
-		}
-
-		logger.config("Time : [" + (System.currentTimeMillis() - startMillis) + "ms]");
-		return result;
+	String[] result = new String[text.length];
+	int count = 0;
+	try {
+	    while (count < text.length) {
+		result[count++] = cmd.getNextResult();
+	    }
+	} catch (InterruptedException | IOException e) {
+	    logger.severe(e.getMessage());
+	    throw e;
+	} finally {
+	    returnInstance(cmd);
 	}
 
-	@Override
+	logger.config("Time : [" + (System.currentTimeMillis() - startMillis) + "ms]");
+	return result;
+    }
+
+    @Override
 	public String getStatus() {
-		String ret = "Command line: " + cmdLine + System.getProperty("line.separator");
-		if(numCmd.get() < poolSize){
-			ret = "Pooled processes: " + ret + numCmd.get() + " / " + poolSize;
-		}else{
-			ret = "Pooled processes: " +  ret + poolSize + " / " + poolSize;
-		}
+	String ret = "Command line: " + cmdLine + System.getProperty("line.separator");
+	if(numCmd.get() < poolSize){
+	    ret = "Pooled processes: " + ret + numCmd.get() + " / " + poolSize;
+	}else{
+	    ret = "Pooled processes: " +  ret + poolSize + " / " + poolSize;
+	}
 
-		return ret + System.getProperty("line.separator");
+	return ret + System.getProperty("line.separator");
+    }
+
+    @Override
+	public void init() {
+	int size = initPoolSize > poolSize ? 1 : initPoolSize;
+	logger.info("init() initSize = " + size);
+	init(size);
+    }
+
+    public void init(int size) {
+	logger.info("init() size = " + size);
+	List<StringIOCommand> list = new ArrayList<StringIOCommand>();
+	try {
+	    for(int i = 0; i < size; i++){
+		StringIOCommand cmd = getInstance();
+		list.add(cmd);
+	    }
+	    for(StringIOCommand cmd : list){
+		returnInstance(cmd);
+	    }
+	} catch (IOException | InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public int getPooledSize() {
+	return numCmd.get();
+    }
+
+    class CatchException implements UncaughtExceptionHandler {
+	public void uncaughtException(Thread t, Throwable e) {
+	    System.out.println(e.getMessage());
+	    e.printStackTrace();
+	}
+    }
+
+    public String getCmdLine() {
+	return cmdLine;
+    }
+
+    public void setCmdLine(String cmdLine) {
+	this.cmdLine = cmdLine;
+    }
+
+    public List<String> getCmdArray() {
+	return cmdArray;
+    }
+
+    public void setCmdArray(List<String> cmdArray) {
+	this.cmdArray = cmdArray;
+    }
+
+    public String getDelimiterIn() {
+	return delimiterIn;
+    }
+
+    public void setDelimiterIn(String delimiterIn) {
+	this.delimiterIn = delimiterIn;
+    }
+
+    public String getDelimiterOut() {
+	return delimiterOut;
+    }
+
+    public void setDelimiterOut(String delimiterOut) {
+	this.delimiterOut = delimiterOut;
+    }
+
+    public boolean isDelLastNewline() {
+	return delLastNewline;
+    }
+
+    public void setDelLastNewline(boolean delLastNewline) {
+	this.delLastNewline = delLastNewline;
+    }
+
+    public boolean isIncludeDelim() {
+	return includeDelim;
+    }
+
+    public void setIncludeDelim(boolean includeDelim) {
+	this.includeDelim = includeDelim;
+    }
+
+    public int getPollTimeOut() {
+	return pollTimeOut;
+    }
+
+    public void setPollTimeOut(int pollTimeOut) {
+	this.pollTimeOut = pollTimeOut;
+    }
+
+    public int getPoolSize() {
+	return poolSize;
+    }
+
+    public void setPoolSize(int poolSize) {
+	this.poolSize = poolSize;
+    }
+
+    public int getInitPoolSize() {
+	return initPoolSize;
+    }
+
+    public void setInitPoolSize(int initPoolSize) {
+	this.initPoolSize = initPoolSize;
+    }
+
+    public int getTimeOut() {
+	return timeOut;
+    }
+
+    public void setTimeOut(int timeOut) {
+	this.timeOut = timeOut;
+    }
+
+    public int getStartWait() {
+	return startWait;
+    }
+
+    public void setStartWait(int startWait) {
+	this.startWait = startWait;
+    }
+
+    public int getRestartWait() {
+	return restartWait;
+    }
+
+    public void setRestartWait(int restartWait) {
+	this.restartWait = restartWait;
+    }
+
+    public int getBufSize() {
+	return bufSize;
+    }
+
+    public void setBufSize(int bufSize) {
+	this.bufSize = bufSize;
+    }
+
+    public String getDirectory() {
+	return directory;
+    }
+
+    public void setDirectory(String directory) {
+	this.directory = directory;
+    }
+
+    private class InputWorker implements Runnable {
+
+	private StringIOCommand cmd;
+	private String[] text;
+
+	public InputWorker(StringIOCommand cmd, String[] text) {
+	    this.cmd = cmd;
+	    this.text = text;
 	}
 
 	@Override
-	public void init() {
-		int size = initPoolSize > poolSize ? 1 : initPoolSize;
-		logger.info("init() initSize = " + size);
-		init(size);
-	}
-
-	public void init(int size) {
-		logger.info("init() size = " + size);
-		List<StringIOCommand> list = new ArrayList<StringIOCommand>();
+	    public void run() {
+	    for (String t : text) {
 		try {
-			for(int i = 0; i < size; i++){
-				StringIOCommand cmd = getInstance();
-				list.add(cmd);
-			}
-			for(StringIOCommand cmd : list){
-				returnInstance(cmd);
-			}
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
+		    cmd.put(t);
+		} catch (IOException e) {
+		    e.printStackTrace();
 		}
+	    }
 	}
-
-	public int getPooledSize() {
-		return numCmd.get();
-	}
-
-	class CatchException implements UncaughtExceptionHandler {
-		public void uncaughtException(Thread t, Throwable e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-	}
-
-	public String getCmdLine() {
-		return cmdLine;
-	}
-
-	public void setCmdLine(String cmdLine) {
-		this.cmdLine = cmdLine;
-	}
-
-	public List<String> getCmdArray() {
-		return cmdArray;
-	}
-
-	public void setCmdArray(List<String> cmdArray) {
-		this.cmdArray = cmdArray;
-	}
-
-	public String getDelimiterIn() {
-		return delimiterIn;
-	}
-
-	public void setDelimiterIn(String delimiterIn) {
-		this.delimiterIn = delimiterIn;
-	}
-
-	public String getDelimiterOut() {
-		return delimiterOut;
-	}
-
-	public void setDelimiterOut(String delimiterOut) {
-		this.delimiterOut = delimiterOut;
-	}
-
-	public boolean isDelLastNewline() {
-		return delLastNewline;
-	}
-
-	public void setDelLastNewline(boolean delLastNewline) {
-		this.delLastNewline = delLastNewline;
-	}
-
-	public boolean isIncludeDelim() {
-		return includeDelim;
-	}
-
-	public void setIncludeDelim(boolean includeDelim) {
-		this.includeDelim = includeDelim;
-	}
-
-	public int getPollTimeOut() {
-		return pollTimeOut;
-	}
-
-	public void setPollTimeOut(int pollTimeOut) {
-		this.pollTimeOut = pollTimeOut;
-	}
-
-	public int getPoolSize() {
-		return poolSize;
-	}
-
-	public void setPoolSize(int poolSize) {
-		this.poolSize = poolSize;
-	}
-
-	public int getInitPoolSize() {
-		return initPoolSize;
-	}
-
-	public void setInitPoolSize(int initPoolSize) {
-		this.initPoolSize = initPoolSize;
-	}
-
-	public int getTimeOut() {
-		return timeOut;
-	}
-
-	public void setTimeOut(int timeOut) {
-		this.timeOut = timeOut;
-	}
-
-	public int getStartWait() {
-		return startWait;
-	}
-
-	public void setStartWait(int startWait) {
-		this.startWait = startWait;
-	}
-
-	public int getRestartWait() {
-		return restartWait;
-	}
-
-	public void setRestartWait(int restartWait) {
-		this.restartWait = restartWait;
-	}
-
-	public int getBufSize() {
-		return bufSize;
-	}
-
-	public void setBufSize(int bufSize) {
-		this.bufSize = bufSize;
-	}
-
-	public String getDirectory() {
-		return directory;
-	}
-
-	public void setDirectory(String directory) {
-		this.directory = directory;
-	}
-
-	private class InputWorker implements Runnable {
-
-		private StringIOCommand cmd;
-		private String[] text;
-
-		public InputWorker(StringIOCommand cmd, String[] text) {
-			this.cmd = cmd;
-			this.text = text;
-		}
-
-		@Override
-		public void run() {
-			for (String t : text) {
-				try {
-					cmd.put(t);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    }
 }
--- jp.go.nict.wisdom.wrapper/src/jp/go/nict/wisdom/wrapper/StandardInputParallelArrayService.java
+++ jp.go.nict.wisdom.wrapper/src/jp/go/nict/wisdom/wrapper/StandardInputParallelArrayService.java
@@ -1,88 +1,97 @@
 /*
-* Copyright (C) 2014 Information Analysis Laboratory, NICT
-*
-* RaSC is free software: you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 2.1 of the License, or (at
-* your option) any later version.
-*
-* RaSC is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
-* General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2014 Information Analysis Laboratory, NICT
+ *
+ * RaSC is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * RaSC is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package jp.go.nict.wisdom.wrapper;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import jp.go.nict.wisdom.daemonizer.command.StandardInputCommand;
 import jp.go.nict.wisdom.daemonizer.command.StringIOCommand;
 
 public class StandardInputParallelArrayService extends AbstractTextAnalysisService {
-	private static Logger logger = Logger.getLogger(StandardInputParallelArrayService.class.getName());
+    private static Logger logger = Logger.getLogger(StandardInputParallelArrayService.class.getName());
 
-	public StandardInputParallelArrayService() {
-		logger.info("new StandardInputArrayParallelService()");
-	}
+    public StandardInputParallelArrayService() {
+	logger.info("new StandardInputArrayParallelService()");
+    }
 
-	@Override
+    @Override
 	protected StringIOCommand getInstance() throws IOException, InterruptedException {
-		return super.getInstance(StandardInputCommand.class);
-	}
+	return super.getInstance(StandardInputCommand.class);
+    }
 
-	@Override
+    @Override
 	public String[] analyzeArray(String[] text) throws Exception {
-		int poolSize = getPoolSize();
-		int execSize = text.length / poolSize;
 
-		if(text.length % poolSize != 0){
-			execSize++;
-		}
-
-		int execNum = text.length < poolSize ? text.length : poolSize;
-		ExecutorService exec = Executors.newFixedThreadPool(execNum);
-		Future<?>[] ft = new Future<?>[execNum];
-
-		for(int i = 0; i < execNum; i++){
-			int from = i * execSize;
-			int to = from + execSize;
-			to = to < text.length ? to : text.length;
-			final String[] str = Arrays.copyOfRange(text, from, to);
-			ft[i] = exec.submit(new Callable<String[]>() {
-				@Override
-				public String[] call() {
-					while(true){
-						try {
-							String[] ret = doAnalyzeArray(str);
-							return ret;
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			});
-		}
-
-		String[] retText = new String[text.length];
-		for(int i = 0; i < execNum; i++){
-			String[] ret = (String[])ft[i].get();
-			System.arraycopy(ret, 0, retText, i * execSize, ret.length);
-		}
-
-		return retText;
+	final LinkedBlockingQueue<NumberedString> queue = new LinkedBlockingQueue<>();
+	for (int i=0; i<text.length; i++) {
+	    queue.add(new NumberedString(i, text[i]));
 	}
 
-	private String[] doAnalyzeArray(String[] text) throws Exception {
-		return super.analyzeArray(text);
+	int poolSize = getPoolSize();
+	int execNum = text.length < poolSize ? text.length : poolSize;
+	ExecutorService exec = Executors.newFixedThreadPool(execNum);
+	Future<?>[] ft = new Future<?>[execNum];
+	final String[] retText = new String[text.length];
+	for(int i = 0; i < execNum; i++){
+	    ft[i] = exec.submit(new Runnable() {
+		    @Override
+			public void run() {
+			while (true) {
+			    try {
+				NumberedString nstr = queue.poll();
+				if (nstr == null) return;
+				String ret = analyze(nstr.str);
+				retText[nstr.num] = ret;
+			    } catch (Exception e) {
+				e.printStackTrace();
+			    }
+			}
+		    }
+		});
 	}
+
+	try {
+	    exec.shutdown();
+	    if(!exec.awaitTermination(timeOut, TimeUnit.MILLISECONDS)){
+		exec.shutdownNow();
+	    }
+	} catch (InterruptedException e) {
+	    logger.warning("Parallel execution didn't finish: " + e);
+	    exec.shutdownNow();
+	}
+
+	return retText;
+    }
+
+    private class NumberedString {
+	public int num;
+	public String str;
+
+	public NumberedString(int num, String str) {
+	    super();
+	    this.num = num;
+	    this.str = str;
+	}
+    }
 }
